54906ba68fc6b90371f58dde19bea9eaf517ae21
Maxim Stykow <46091730+mstykow@users.noreply.github.com>
2019-01-06 23:32:13 -0800

    Optimized code

--- ProtonMailer.py
+++ ProtonMailer.py
@@ -6,6 +6,29 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
+def ProtonMailer(username, password, recipient, subject, message):
+    try:
+        browser = webdriver.Chrome()
+        browser.get('https://mail.protonmail.com/login')
+        usernameElem = browser.find_element_by_id('username')
+        passwordElem = browser.find_element_by_id('password')
+        usernameElem.send_keys(username)
+        passwordElem.send_keys(password)
+        browser.find_element_by_id('login_btn').click()
+        sleep(15)
+        browser.find_element_by_css_selector('section button').click()
+        sleep(5)
+        browser.switch_to.active_element.send_keys(recipient + Keys.ENTER + '\t')
+        sleep(2)
+        browser.switch_to.active_element.send_keys(subject + '\t' + message + '\t' + '\t' + '\t' + '\t' + '\t' + '\t' + Keys.ENTER)
+        sleep(5)
+        browser.quit()
+        print('Email sent!')
+    except Exception as err:
+        browser.quit()
+        print('An error occurred while sending email:')
+        print(str(err))
+
 print('Welcome to ProtonMail.')
 print('Please enter your username:')
 username = input()
@@ -18,24 +41,4 @@ subject = input()
 print('Please enter the message:')
 message = input()
 
-try:
-    browser = webdriver.Chrome()
-    browser.get('https://mail.protonmail.com/login')
-    usernameElem = browser.find_element_by_id('username')
-    passwordElem = browser.find_element_by_id('password')
-    usernameElem.send_keys(username)
-    passwordElem.send_keys(password)
-    browser.find_element_by_id('login_btn').click()
-    sleep(15)
-    browser.find_element_by_css_selector('section button').click()
-    sleep(5)
-    browser.switch_to.active_element.send_keys(recipient + Keys.ENTER + '\t')
-    sleep(2)
-    browser.switch_to.active_element.send_keys(subject + '\t' + message + '\t' + '\t' + '\t' + '\t' + '\t' + '\t' + Keys.ENTER)
-    sleep(5)
-    browser.quit()
-    print('Email sent!')
-except Exception as err:
-    browser.quit()
-    print('An error occurred while sending email:')
-    print(str(err))
+ProtonMailer(username, password, recipient, subject, message)
