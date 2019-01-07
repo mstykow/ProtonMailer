cc6dc8f47d807a0631f91410445dec029a67a54b
Maxim Stykow <46091730+mstykow@users.noreply.github.com>
2019-01-06 22:56:38 -0800

    Add files via upload

---
+++ ProtonMailer.py
@@ -0,0 +1,41 @@
+#! python3
+# Program prompts user for ProtonMail credentials and sends message to a select
+# recipient with specified subject.
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+print('Welcome to ProtonMail.')
+print('Please enter your username:')
+username = input()
+print('Please enter your password:')
+password = input()
+print("Please enter the recipient's email address:")
+recipient = input()
+print('Please enter the subject:')
+subject = input()
+print('Please enter the message:')
+message = input()
+
+try:
+    browser = webdriver.Chrome()
+    browser.get('https://mail.protonmail.com/login')
+    usernameElem = browser.find_element_by_id('username')
+    passwordElem = browser.find_element_by_id('password')
+    usernameElem.send_keys(username)
+    passwordElem.send_keys(password)
+    browser.find_element_by_id('login_btn').click()
+    sleep(15)
+    browser.find_element_by_css_selector('section button').click()
+    sleep(5)
+    browser.switch_to.active_element.send_keys(recipient + Keys.ENTER + '\t')
+    sleep(2)
+    browser.switch_to.active_element.send_keys(subject + '\t' + message + '\t' + '\t' + '\t' + '\t' + '\t' + '\t' + Keys.ENTER)
+    sleep(5)
+    browser.quit()
+    print('Email sent!')
+except Exception as err:
+    browser.quit()
+    print('An error occurred while sending email:')
+    print(str(err))
